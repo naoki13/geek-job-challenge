3df9a6dce1be661a874986647ee599def6262050
naoki13 <nero13@gmail.com>
2017-03-08 14:06:35 +0900

    servlet2-9

---
+++ CampChallenge_java/CampChallenge_java/004.JSPとサーブレット２/Servlet2-9.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(name = "Servlet2", urlPatterns = {"/Servlet2"})
+public class Servlet2 extends HttpServlet {
+    
+Integer limit=2;
+    
+    String getPro1(){
+    String name1="山田";
+    String birth1="1.23";
+    String ad1="東京";
+    String pro1=name1+"<br>"+birth1+"<br>"+ad1+"<br>"+"<br>";
+    return pro1;
+}
+
+    String getPro2(){
+    String name2="田中";
+    String birth2="4.5";
+    String ad2="大阪";
+    String pro2=name2+"<br>"+birth2+"<br>"+ad2+"<br>"+"<br>";
+    return pro2;
+}
+    String getPro3(){
+    String name3="鈴木";
+    String birth3="6.7";
+    String ad3=null;
+    String pro3=name3+"<br>"+birth3+"<br>"+ad3+"<br>"+"<br>";
+    return pro3;
+}
+
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet2</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            String A=getPro1();
+            String B=getPro2();
+            String C=getPro3();
+            
+            ArrayList<String>al=new ArrayList<>();
+            al.add(A);
+            al.add(B);
+            al.add(C);
+            
+            for(int i=limit;i<al.size();i++){
+                if(al.get(i)== null){
+                continue;
+                }
+            out.print(al.get(i));
+            }
+            out.println("</body>");
+            out.println("</html>");
+        
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
