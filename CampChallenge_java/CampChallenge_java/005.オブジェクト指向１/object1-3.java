869abdbc22981bd8396d3ee013b2d4e8961fa868
naoki13 <nero13@gmail.com>
2017-03-10 13:29:06 +0900

    object1-3

---
+++ CampChallenge_java/CampChallenge_java/005.オブジェクト指向１/object1-3.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(name = "object", urlPatterns = {"/object"})
+public class object extends HttpServlet {
+    
+    class Player{
+        public int back=0;
+        public int age= 0;
+        public void getAge(int age){
+            this.age=age;
+        }
+        public void getBack(int back){
+            this.back=back;
+        }
+        public void printAge(PrintWriter out){
+            out.print(age);
+        }
+        public void printBack(PrintWriter out){
+            out.print(back);
+        }
+        
+    }   
+    
+    
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet object</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            Player Pogba=new Player();
+            Pogba.getAge(22);
+            Pogba.getBack(19);
+            Pogba.printAge(out);
+            out.print("<br>");
+            Pogba.printBack(out);
+            
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
